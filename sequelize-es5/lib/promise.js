24d91d5bbb3e875e580bb4b83f97957ff8e4090a
Rodrigo Pavez <rpavez@gmail.com>
2017-05-18 16:19:22 -0400

    removed source folders

--- sequelize-es5/lib/promise.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var Promise = require('bluebird').getNewLibraryCopy();
-
-module.exports = Promise;
-module.exports.Promise = Promise;
-module.exports.default = Promise;
-//# sourceMappingURL=promise.js.map
